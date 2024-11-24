1a67a1b46e386b83e4a9ef044aad684d68748ca7
arssekal <arssekall@gmail.com>
2024-11-24 15:47:11 +0100

    finicjing the count project

--- questions/script.js
+++ questions/script.js
@@ -1,4 +1,20 @@
 const plusIcon = document.querySelectorAll(".plus-icon");
 const minusIcon = document.querySelectorAll(".minus-icon");
 const questionText = document.querySelectorAll(".question-text");
-
+const questions = document.querySelectorAll(".question");
+plusIcon.forEach(function(elem, idx, arr) {
+    elem.addEventListener("click", function() {
+        questions.forEach(function(el, index){
+            if (index === idx) {
+                el.classList.toggle("show-text");
+            } else {
+                el.classList.remove("show-text");
+            }
+        })
+    })
+})
+minusIcon.forEach(function(elem, idx, arr) {
+    elem.addEventListener("click", function() {
+        questions[idx].classList.toggle("show-text");
+    });
+})
